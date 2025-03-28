69deb65195c7dbb5530297d42e8805375cbdbe05
henrikschimmelbusch1 <161975850+henrikschimmelbusch1@users.noreply.github.com>
2025-03-28 16:29:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,6 @@
 // PASTE YOUR FIREBASE CONFIG SNIPPET HERE:
 // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
-
-import { getAnalytics } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
   apiKey: "AIzaSyDB0jlo4Dj1NF6uv4rAU-uhzVH59ZHov4E",
   authDomain: "my-live-tictactoe.firebaseapp.com",
